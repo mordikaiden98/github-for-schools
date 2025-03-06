1892e524397005280ca6460ca05fafe64bfdf0d2
mordikaiden98 <mordikaiden98@gmx.com>
2025-03-06 09:53:34 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int age = 25;
+  if (age >= 18) {
+    printf("You can vote!\n");
+  } else {
+    printf("Sorry, you are not eligible to vote yet.\n");
+  }
+  return 0;
+}
